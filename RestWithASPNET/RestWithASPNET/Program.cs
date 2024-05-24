098021d76fddda36cd2d9c83f9baf42a287ff8fe
Pedro Henrique Zaccaria <pedrozaccaria@gmail.com>
2024-05-24 09:13:18 -0300

    No mocks anymore

--- RestWithASPNET/RestWithASPNET/Program.cs
+++ RestWithASPNET/RestWithASPNET/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services;
+using RestWithASPNET.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 29))));
+// Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
